3ff78cefb9f56a502d73bd4d0db5cb5b2e37a400
RoxanaCamara <roxana.camara07@gmail.com>
2025-08-21 00:25:19 -0300

    feat: maquetado error

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import "./App.css";
 import illustrationSignUpMobile from "./assets/illustration-sign-up-mobile.svg";
 
 import iconList from "./assets/icon-list.svg";
+import { useState } from "react";
 
 //import illustrationSignUpTablet from "./assets/illustration-sign-up-tablet.svg";
 //import illustrationSignUDesktop from "./assets/illustration-sign-up-desktop.svg";
 
 function App() {
+  const [valid, setValid] = useState(false);
+
   return (
     <>
       <div className="flex items-start justify-center min-h-screen">
@@ -58,21 +61,28 @@ function App() {
             </ul>
           </div>
 
-          <div className="flex flex-col">
+          <div className="flex flex-row justify-between">
             <span className="text-[Roboto] text-[12px] text-[#242742] font-medium p-1">
               Email address
             </span>
-            <input
-              type="text"
-              className="w-full p-4 text-[Roboto] text-[16px] text-[#949494] rounded-lg border border-solid border-[#949494]"
-              placeholder={"email@company.com"}
-            />
-            <button className="w-full bg-[#242742] p-4 mt-6 rounded-lg">
-              <p className="text-[Roboto] text-[16px] text-white font-medium">
-                Subscribe to monthly newsletter
-              </p>
-            </button>
+
+            <span className="text-[Roboto] text-[12px] text-[#FF6155]  font-medium p-1">
+              Valid email required
+            </span>
           </div>
+          <input
+            type="text"
+            className="w-full p-4 text-[Roboto] text-[16px] text-[#949494] rounded-lg border border-solid border-[#949494]"
+            placeholder={"email@company.com"}
+          />
+          <button
+            onClick={() => setValid(!valid)}
+            className="w-full bg-[#242742] p-4 mt-6 rounded-lg"
+          >
+            <p className="text-[Roboto] text-[16px] text-white font-medium">
+              Subscribe to monthly newsletter
+            </p>
+          </button>
         </div>
       </div>
     </>
